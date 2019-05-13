53d35fd38572db6fef9451a21fae350b330e7813
Hakumai <inside.rice164@gmail.com>
2019-05-13 22:39:49 +0900

    Initial commit with maven project template

---
+++ src/main/java/net/insidehakumai/jderanger/App.java
@@ -0,0 +1,13 @@
+package net.insidehakumai.jderanger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
